c3febecc5d2a72b1b7e7986e825bd01b5fe1c972
itadigital <csaldanha@itagroup.com>
2023-10-05 15:50:46 -0500

    Update get-data.php

--- get-data.php
+++ get-data.php
@@ -6,9 +6,9 @@ header('Content-Type: application/json');
 $request = new HttpRequest();
 $request->setUrl('https://eu1.make.com/api/v2/scenarios/1509723/run');
 $request->setMethod(HTTP_METH_POST);
-$request->setHeaders([  'Authorization' => 'Token 86cd309b-92ad-4025-965e-dcd15c9c2e62']);
-$request->setBody('{"data":{"personId":"7010c621-c5ad-46bc-8ed4-0969347b44d7"},
-                    "responsive":false}');
+$request->setHeaders(['Authorization' => 'Token 86cd309b-92ad-4025-965e-dcd15c9c2e62']);
+/*$request->setBody('{"data":{"personId":"7010c621-c5ad-46bc-8ed4-0969347b44d7"},
+                    "responsive":false}');*/
 try {  $response = $request->send();  
      echo $response->getBody();
     } 
