fb8a2ee4f76c701503ba9f0d90f4765cc6c0d086
itadigital <csaldanha@itagroup.com>
2023-10-05 15:45:53 -0500

    Update get-data.php

--- get-data.php
+++ get-data.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
-$request = new HttpRequest();
+/*$request = new HttpRequest();
 $request->setUrl('https://eu1.make.com/api/v2/scenarios/1509723/run');
 $request->setMethod(HTTP_METH_POST);
 $request->setHeaders([  'Authorization' => 'Token 86cd309b-92ad-4025-965e-dcd15c9c2e62']);
@@ -13,6 +13,24 @@ try {  $response = $request->send();
     } 
 catch (HttpException $ex) { 
     echo $ex;
+}*/
+
+$request = new HttpRequest();
+$request->setUrl('https://eu1.make.com/api/v2/scenarios/111/run');
+$request->setMethod(HTTP_METH_POST);
+
+$request->setHeaders([
+  'Authorization' => 'Token abcdefab-1234-5678-abcd-112233445566'
+]);
+
+$request->setBody('{"data":{"Test input":"Test value","My array":["test 1","test 2"],"My collection":{"key":"value"}},"responsive":false}');
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
 }
 
 ?>
