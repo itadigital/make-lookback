5f636ece8e2207d6fb1b5200c074c60a7628cac3
itadigital <csaldanha@itagroup.com>
2023-10-06 12:42:06 -0500

    Update get-data.php

--- get-data.php
+++ get-data.php
@@ -6,6 +6,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $service_url = 'https://itagroup.hs.llnwd.net/o40/csg/year-end-recap/lookback-2023-vimeo.json';
+$person = $_GET['personId'];
+
 $curl = curl_init($service_url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 $curl_response = curl_exec($curl);
@@ -23,21 +25,25 @@ if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') 
 }
 
 foreach ($decoded as $item) {
-    
-    $myObj = array(
-        "personid" => $item['personid'],
-        "first-name" => $item['first-name'],
-        "number-sent-recognitions" => $item['number-sent-recognitions'],
-        "number-received-recognitions" => $item['number-received-recognitions'],
-        "total-value1-received" => $item['total-value1-received'],
-        "total-value2-received" => $item['total-value2-received'],
-        "total-value3-received" => $item['total-value3-received'],
-        "total-value4-received" => $item['total-value4-received'],
-        "total-value5-received" => $item['total-value5-received'],
-        "total-company-recognitions-sent" => $item['total-company-recognitions-sent'] 
-    );
-  
-    $personItems[] = $myObj;
+
+    if ($item['personid'] == $person) {
+        $myObj = array(
+            "personid" => $item['personid'],
+            "first-name" => $item['first-name'],
+            "number-sent-recognitions" => $item['number-sent-recognitions'],
+            "number-received-recognitions" => $item['number-received-recognitions'],
+            "total-value1-received" => $item['total-value1-received'],
+            "total-value2-received" => $item['total-value2-received'],
+            "total-value3-received" => $item['total-value3-received'],
+            "total-value4-received" => $item['total-value4-received'],
+            "total-value5-received" => $item['total-value5-received'],
+            "total-company-recognitions-sent" => $item['total-company-recognitions-sent'] 
+        );
+      
+        $personItems[] = $myObj;
+
+        endforeach;
+    }
     
 }
 
